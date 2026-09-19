3c51d9ed07afdc8548f6bd3ae319f9e7a7ca1c5d
khujisherpur <khujisherpur@gmail.com>
2026-09-19 17:23:44 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ink: '#1B2A4A',
+        paper: '#F5F7F4',
+        green: {
+          DEFAULT: '#1C8A4C',
+          dark: '#0E5C33',
+        },
+        marigold: '#E3A72E',
+      },
+      fontFamily: {
+        sans: ['var(--font-hind)', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
