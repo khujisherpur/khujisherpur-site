c661533d94d4386fe2dc6951feddc3303408f20a
khujisherpur <khujisherpur@gmail.com>
2026-09-22 17:15:59 +0600

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,6 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { supabase } from '../../lib/supabaseClient';
-import { supabase } from '../../lib/supabaseClient';
 import NotificationBell from '../../components/NotificationBell';
 import BottomNav from '../../components/BottomNav';
 
